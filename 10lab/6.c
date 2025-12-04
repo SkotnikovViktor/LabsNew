10f5222dcffb72de2b0d56676219fc8a898aeaeb
viktor <skotnikovviktor82@gmail.com>
2025-12-04 22:19:05 +0300

    Fix: Исправлена ошибка в перевеоде из binary в decimal

--- 10lab/6.c
+++ 10lab/6.c
@@ -9,52 +9,55 @@ int binary_to_decimal(int start, int end, int *massive_binary){
 
     int number_pow2 = 1;
     int result_decimal = 0;
-    for (int i = start; i <= end; i++){
+    for (int i = end; i >= start; i--){
+        printf("%d\n",massive_binary[i]);
         if (massive_binary[i] == 1){
             result_decimal += number_pow2;
         }
         number_pow2 *= 2;
     }
+    printf("\n");
 
     return result_decimal;
 }
 
-int obrabotka(int a, int b, int c, int type_operation, int kod_operation){
+int obrabotka(int a, int b, int type_operation, int kod_operation){
 
     switch (type_operation)
     {
 
     case 0: 
         if (kod_operation == 0){
-            return (a + b + c);
+            return (a + b);
+
 
         } else if (kod_operation == 1){
-            return (a - b - c);
+            return (a - b);
 
-        } else if (kod_operation == 10){
-            return (a / b / c);
+        } else if (kod_operation == 2){
+            return (a / b);
 
-        } else if (kod_operation == 11){
-            return (a * b * c);
+        } else if (kod_operation == 3){
+            return (a * b);
         }
         break;
     
 
     case 1:
-        if (kod_operation == 100){
-            return (a << b << c);
+        if (kod_operation == 4){
+            return (a << b);
 
-        } else if (kod_operation == 101){
-            return (a >> b >> c);
+        } else if (kod_operation == 5){
+            return (a >> b);
         }
         break;
 
-    case 10:
-        if (kod_operation == 110){
-            return (a && b && c);
+    case 2:
+        if (kod_operation == 6){
+            return (a && b);
 
-        } else if (kod_operation == 111){
-            return (a || b || c);
+        } else if (kod_operation == 7){
+            return (a || b);
         }
         break;
 
@@ -112,9 +115,9 @@ int main(void){
     kod_operation = binary_to_decimal(2,5,massive_binary);
     a = binary_to_decimal(6,9,massive_binary);
     b = binary_to_decimal(10,13,massive_binary);
-    c = binary_to_decimal(27,31,massive_binary);
 
-    printf("%d", obrabotka(a,b,c,type_operation,kod_operation));
+
+    printf("%d", obrabotka(a,b,type_operation,kod_operation));
 
 
 
